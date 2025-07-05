ebb792cb24530e9041943da90a8e75655f53020c
kelly <kelly.neuroth@outlook.com>
2025-07-05 09:43:23 -0400

    Preprocess post for frontend ease

--- model/Score.ts
+++ model/Score.ts
@@ -12,10 +12,10 @@ export const scoreSchema = z.object({
 export type Score = z.infer<typeof scoreSchema>
 
 export const insertScoreSchema = z.object({
-  player_id: z.number(),
+  player_id: z.preprocess(val => Number(val), z.number()),
   player_name: z.string(),
-  chat_id: z.number(),
-  score: z.number(),
+  chat_id: z.preprocess(val => Number(val), z.number()),
+  score: z.preprocess(val => Number(val), z.number()),
   date: z.string()
 })
 
