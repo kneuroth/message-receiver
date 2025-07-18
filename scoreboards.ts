65b38ae50c89e0f9c5f8f4433dbb695cb16688ff
kelly <kelly.neuroth@outlook.com>
2025-07-17 21:45:27 -0400

    Cleanup unused deps

--- scoreboards.ts
+++ scoreboards.ts
@@ -1,30 +1,12 @@
 import { APIGatewayProxyResult } from 'aws-lambda'
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb'
 import { convertScoresToScoreboards } from './util/logic'
 import { scoreSchema } from './model/Score'
 
 module.exports.getRawScoreboards = async (): Promise<APIGatewayProxyResult> => {
-  const client = DynamoDBDocumentClient.from(new DynamoDBClient({}))
-  const command = new ScanCommand({
-    TableName: process.env.DYNAMODB_SCORE_TABLE,
-  })
-  const response = await client.send(command)
-
-  const scoreParse = scoreSchema.array().safeParse(response.Items)
-  if (!scoreParse.success) {
-    console.error('Invalid scores:', response.Items)
-    console.error('Error:', scoreParse.error.issues)
-    return {
-      statusCode: 400,
-      body: 'Invalid score objects',
-    }
-  }
-
 
   return {
     statusCode: 200,
-    body: JSON.stringify(convertScoresToScoreboards(scoreParse.data)),
+    body: 'guh'
   }
 
 }
